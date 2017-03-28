536c1fb19d9c45fc65c89247c0860ca1f9a4a88e
Josh Gavant <josh.gavant@outlook.com>
2017-03-28 22:05:21 +0200

    push .env vars to web apps
    also check for an existing git remote named 'azure'

--- scripts/webapp-deploy.sh
+++ scripts/webapp-deploy.sh
@@ -5,7 +5,7 @@ set -o allexport
 source ${SCRIPT_DIR}/../.env
 set +o allexport
 
-exists=$(az group exists --name ${GROUP_NAME})
+exists=$(az group exists --name ${GROUP_NAME} --output json)
 echo $GROUP_NAME exists: $exists
 if [ $exists == 'false' ]; then
   az group create \
@@ -39,8 +39,16 @@ az appservice web deployment user set \
   --user-name ${WEBAPPS_DEPLOY_USER} \
   --password ${WEBAPPS_DEPLOY_PASSWORD}
 
-git remote add azure https://${WEBAPPS_DEPLOY_USER}:${WEBAPPS_DEPLOY_PASSWORD}@${WEBAPPS_SITE_NAME}.scm.azurewebsites.net/${WEBAPPS_SITE_NAME}.git
-git push azure master
+az appservice web config appsettings update \
+  --resource-group ${GROUP_NAME} \
+  --name ${WEBAPPS_SITE_NAME} \
+  --settings $(cat ${SCRIPT_DIR}/../.env | grep '^[A-Z]' | tr '\n' ' ')
+
+if [ -e $(git remote | grep azure) ]; then
+  git remote add azure https://${WEBAPPS_DEPLOY_USER}:${WEBAPPS_DEPLOY_PASSWORD}@${WEBAPPS_SITE_NAME}.scm.azurewebsites.net/${WEBAPPS_SITE_NAME}.git
+fi
+
+git push azure master:master
 
 az appservice web browse \
   --name ${WEBAPPS_SITE_NAME} \
