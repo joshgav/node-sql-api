b036e47da89e04cca8299c8db761764d63738e97
Josh Gavant <joshgavant@gmail.com>
2018-02-01 13:51:17 -0800

    update az CLI commands

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
-A simple app which connects to SQL with [tedious][] and surfaces objects via an Express API.
+A simple app which connects to SQL with [tedious][] and surfaces records as
+objects via an Express API.
 
 [tedious]: https://github.com/tediousjs/tedious
 
-### Caveats
+## Caveats
 
-* Assumes DATABASE\_NAME populated with AdventureWorksLT sample database.
+* Make sure the SQL server firewall allows access to the web server.
+* Assumes databse is populated with AdventureWorksLT sample database.
   * Instructions [here](https://docs.microsoft.com/en-us/azure/sql-database/sql-database-get-started-portal).
-* Uses SQL Authentication (username:password) for easy compat with Azure SQL.
+  * Also an option when creating a SQL DB in the portal.
+* Use SQL Authentication (username:password) for easy compat with Azure SQL.
 
-### Setup
+## Setup
 
-1. Create a `.env` file in root of repo with at least the following key:value pairs:
+1. Copy .env.template to .env in your own repo and populate the variables.
+   The Azure ones are for use with `scripts/webapp-deploy.sh`.
+1. `npm install`
+1. `node .` (from repo root).
+1. Browse or curl `http://localhost:3000/customers`.
 
-```bash
-SQL_USERNAME=_username_
-SQL_PASSWORD=_password_
-SQL_HOSTNAME=_hostname_.database.windows.net
-SQL_DBNAME=_database_name_
-```
+Alternatively, deploy the web app to Azure Web Apps, populate App Settings with
+the same env vars, and open the SQL server firewall.
 
-2. `npm install`
-3. `node .` (from repo root).
-4. Browse or curl `http://localhost:3000/customers`.
+## License
+
+See [LICENSE.md](./LICENSE.md).
+
--- scripts/webapp-deploy.sh
+++ scripts/webapp-deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 
 set -o allexport
@@ -13,43 +13,26 @@ if [ $exists == 'false' ]; then
     --location ${DEFAULT_LOCATION}
 fi
 
-az appservice plan create \
-  --sku S1 \
-  --resource-group ${GROUP_NAME} \
-  --name ${WEBAPPS_PLAN_NAME} \
-  --location ${DEFAULT_LOCATION} \
-  --is-linux
+# az appservice plan create \
+#   --sku S1 \
+#   --resource-group ${GROUP_NAME} \
+#   --name ${WEBAPPS_PLAN_NAME} \
+#  --location ${DEFAULT_LOCATION} \
+#  --is-linux
 
-az appservice web create \
+az webapp create \
   --resource-group ${GROUP_NAME} \
   --plan ${WEBAPPS_PLAN_NAME} \
-  --name ${WEBAPPS_SITE_NAME}
-
-az appservice web config update \
-  --resource-group ${GROUP_NAME} \
   --name ${WEBAPPS_SITE_NAME} \
-  --node-version ${WEBAPPS_NODE_VERSION} \
-  --startup-file ${WEBAPPS_NODE_MAIN}
+  --deployment-source-url 'https://github.com/joshgav/node-sql-api' \
+  --runtime 'node|8.1' \
+  --startup-file 'index.js'
 
-az appservice web source-control config-local-git \
-  --resource-group ${GROUP_NAME} \
-  --name ${WEBAPPS_SITE_NAME}
-
-az appservice web deployment user set \
-  --user-name ${WEBAPPS_DEPLOY_USER} \
-  --password ${WEBAPPS_DEPLOY_PASSWORD}
-
-az appservice web config appsettings update \
+az webapp config appsettings set \
   --resource-group ${GROUP_NAME} \
   --name ${WEBAPPS_SITE_NAME} \
   --settings $(cat ${SCRIPT_DIR}/../.env | grep '^[A-Z]' | tr '\n' ' ')
 
-if [ -e $(git remote | grep azure) ]; then
-  git remote add azure https://${WEBAPPS_DEPLOY_USER}:${WEBAPPS_DEPLOY_PASSWORD}@${WEBAPPS_SITE_NAME}.scm.azurewebsites.net/${WEBAPPS_SITE_NAME}.git
-fi
-
-git push azure master:master
-
-az appservice web browse \
+az webapp browse \
   --name ${WEBAPPS_SITE_NAME} \
   --resource-group ${GROUP_NAME}
